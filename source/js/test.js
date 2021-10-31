7e7a6d21d5e146d1df42c6ecfddb109d6d606b15
Евкакий <buligani@mail.ru>
2021-10-31 20:27:38 +0300

    31.10.2021

---
+++ source/js/test.js
@@ -0,0 +1,37 @@
+// for(let i = 1; i < 11; i++) {
+//   if (i % 2 == 1) continue;
+
+//   alert(i)
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   alert( `number ${i}!` );
+// }
+
+// let i = 0;
+// while(i < 3) {
+//   alert( `number ${i}!` );
+//   i++;
+// }
+
+// for (;;) {
+//   let value = prompt('Введите число больше 100: ', 0)
+//   if (value > 100 || !value) {
+//     break
+//   }
+// }
+
+// let num;
+
+// do {
+//   num = prompt('Введите число больше 100: ', 0)
+// } while (num <=100 && num)
+
+// let n = 21
+// nextPrime:
+// for (let i = 2; i <= n; i++) {
+//   for(let j = 2; j < i; j++) {
+//     if (i % j == 0) continue nextPrime;
+//   }
+//   alert(i)
+// }
